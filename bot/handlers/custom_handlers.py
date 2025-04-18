86b3ac05f898e394b33120a603d1344814ae67cb
Andrey Bokarev <gagamen2004@gmail.com>
2025-04-18 12:35:25 +0300

    Add API handlers

---
+++ bot/handlers/custom_handlers.py
@@ -0,0 +1,24 @@
+import logging
+
+from aiogram import Router
+from aiogram.filters import Command
+from aiogram.types import Message
+
+from api.api import get_json_response
+from config.config import API_URL
+
+custom_router = Router(name='custom_router')
+
+
+@custom_router.message(Command(commands=['get']))
+async def get_request_handler(message: Message):
+    await message.answer('Отправляю запрос и собираю данные..')
+
+    try:
+        logging.info(f'Отправляю запрос на URL {API_URL + 'posts/1'}')
+        data = await get_json_response(API_URL, 'posts/1')
+    except Exception as e:
+        logging.error(f'Произошла ошибка при запросе:\n{e}')
+        await message.answer('Произошла ошибка при запросе, проверьте логи')
+    else:
+        await message.answer(f'Было получен ответ JSON:\n{data}')
