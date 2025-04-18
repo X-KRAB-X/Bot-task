e5d0a2cf09f91b3c617e26f3b4f4d8f8a7fdc986
X-KRAB-X <84371405+X-KRAB-X@users.noreply.github.com>
2025-04-18 11:53:57 +0300

    Configured Webhook
    * Log setting webhook
    
    * Change path in handler
    
    * get webhook info
    
    * register webhook
    
    * getting webhook-info
    
    * Add logs and docs

--- bot/loader.py
+++ bot/loader.py
@@ -1,4 +1,5 @@
 from aiohttp import web
+import logging
 
 from aiogram import Dispatcher, Router, Bot
 from aiogram.client.default import DefaultBotProperties
@@ -29,30 +30,40 @@ dp = Dispatcher()
 dp.include_router(main_router)
 
 
-async def _on_startup(bot_instance: Bot) -> None:
+async def _set_webhook(bot_instance: Bot) -> None:
     """
     Функция для инициализации webhook'а боту.
 
     :param bot_instance: Текущий бот
     """
+    try:
+        await bot_instance.set_webhook(f'{WEBHOOK_URL}{WEBHOOK_PATH}')
+        logging.info(f'Установлен путь для вебхука: {WEBHOOK_URL}{WEBHOOK_PATH}')
+    except Exception as e:
+        logging.error(f'Произошла ошибка при установке вебхука:\n{e}')
 
-    await bot_instance.set_webhook(f'{WEBHOOK_URL}/{WEBHOOK_PATH}')
 
-
-async def _on_shutdown(bot_instance: Bot) -> None:
+async def clear_webhook(bot_instance: Bot) -> None:
     """
     Функция для очистки webhook'а у бота.
 
     :param bot_instance: Текущий бот
     """
+    logging.info(f'Установлен путь для вебхука: {WEBHOOK_URL}{WEBHOOK_PATH}')
+    try:
+        await bot_instance.delete_webhook(drop_pending_updates=True)
+    except Exception as e:
+        logging.error(f'Произошла ошибка при очистке вебхука:\n{e}')
 
-    await bot_instance.delete_webhook(drop_pending_updates=True)
 
 async def loader() -> web.AppRunner:
+    """
+    Сборка и настройка всех частей бота:
+    Веб-приложение и Вебхук для бота
+    """
 
     # Инициализируем webhook
-    dp.startup.register(_on_startup)
-    dp.shutdown.register(_on_shutdown)
+    await _set_webhook(bot_instance=bot)
 
     # Создаем веб-приложение
     app = web.Application()
--- bot/main.py
+++ bot/main.py
@@ -1,17 +1,22 @@
 import asyncio
 import logging
 
-from loader import loader, bot
+from loader import loader, bot, clear_webhook
 
 
 async def main():
     # await dp.start_polling(bot)
     runner = await loader()
+    print(await bot.get_webhook_info())
+
     try:
         await asyncio.Future()
     finally:
         await runner.cleanup()
+        # Очищаем вебхук, на всякий случай
+        await clear_webhook(bot_instance=bot)
         await bot.session.close()
+
 
 if __name__ == '__main__':
     logging.basicConfig(level='INFO')
