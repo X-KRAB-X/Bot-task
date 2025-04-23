8432007c80ccbb497b68e22e3ff56ded219a46f9
Andrey Bokarev <gagamen2004@gmail.com>
2025-04-23 19:39:12 +0300

    Created all models

--- bot/models/pydantic_api.py
+++ bot/models/pydantic_api.py
@@ -4,12 +4,29 @@
 """
 
 # Todo Сделать валидацию для многих полей. Для телефона и email использовать regex
+from datetime import datetime
+
 from pydantic import BaseModel, validator, Field, ConfigDict
+
+from bot.models.db import PhotoDBModel
+
+
+class _DBMixin(BaseModel):
+    """
+    Класс-примесь
+    Содержит поля, которые создаются в БД
+    """
+
+    id: int
+    telegram_user_id: int
+    created_at: datetime
 
 
 class _UsersAddressGeoModel(BaseModel):
     lat: float
     lng: float
+
+    model_config = ConfigDict(from_attributes=True)
 
 
 class _UsersAddressModel(BaseModel):
@@ -19,15 +36,19 @@ class _UsersAddressModel(BaseModel):
     zipcode: str
     geo: _UsersAddressGeoModel
 
+    model_config = ConfigDict(from_attributes=True)
+
 
 class _UsersCompanyModel(BaseModel):
     name: str
     catchPhrase: str
     bs: str
 
+    model_config = ConfigDict(from_attributes=True)
 
-class UsersModel(BaseModel):
-    id: int
+
+class UserModel(BaseModel):
+    user_id: int = Field(alias='id')
     name: str
     username: str
     email: str
@@ -36,9 +57,15 @@ class UsersModel(BaseModel):
     website: str
     company: _UsersCompanyModel
 
+    model_config = ConfigDict(from_attributes=True)
+
+
+class UserModelFromDB(UserModel, _DBMixin):
+    user_id: int = Field() # Лишаем поле alias='id' во избежание конфликтов
+
 
 class PostModel(BaseModel):
-    id: int
+    post_id: int = Field(alias='id')
     user_id: int
     title: str
     body: str
@@ -46,30 +73,58 @@ class PostModel(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
 
+class PostModelFromDB(PostModel, _DBMixin):
+    post_id: int = Field() # Лишаем поле alias='id' во избежание конфликтов
+
+
 class CommentsModel(BaseModel):
     post_id: int
-    id: int
+    comment_id: int = Field(alias='id')
     name: str
     email: str
     body: str
 
+    model_config = ConfigDict(from_attributes=True)
 
-class AlbumsModel(BaseModel):
+
+class CommentModelFromDB(CommentsModel, _DBMixin):
+    comment_id: int = Field() # Лишаем поле alias='id' во избежание конфликтов
+
+
+class AlbumModel(BaseModel):
     user_id: int
-    id: int
+    album_id: int = Field(alias='id')
     title: str
 
+    model_config = ConfigDict(from_attributes=True)
 
-class PhotosModel(BaseModel):
-    albumId: int
-    id: int
+
+class AlbumModelFromDB(AlbumModel, _DBMixin):
+    album_id: int = Field() # Лишаем поле alias='id' во избежание конфликтов
+
+
+class PhotoModel(BaseModel):
+    album_id: int
+    photo_id: int = Field(alias='id')
     title: str
     url: str
-    thumbnailUrl: str
+    thumbnail_url: str
+
+    model_config = ConfigDict(from_attributes=True)
 
 
-class TodosModel(BaseModel):
-    userId: int
-    id: int
+class PhotoModelFromDB(PhotoModel, _DBMixin):
+    photo_id: int = Field() # Лишаем поле alias='id' во избежание конфликтов
+
+
+class TodoModel(BaseModel):
+    user_id: int
+    todo_id: int = Field(alias='id')
     title: str
     completed: bool
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class TodoModelFromDB(TodoModel, _DBMixin):
+    todo_id: int = Field() # Лишаем поле alias='id' во избежание конфликтов
