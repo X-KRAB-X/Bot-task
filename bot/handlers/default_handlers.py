1de9eca695d5a8c1d8d6f69642f3b32fe74dc6c0
Andrey Bokarev <gagamen2004@gmail.com>
2025-04-24 19:52:33 +0300

    bug fixed

--- bot/handlers/default_handlers.py
+++ bot/handlers/default_handlers.py
@@ -7,17 +7,17 @@ from aiogram.types import Message
 default_router = Router(name='default_router')
 
 
-@default_router.message.register(Command(commands=['start']), StateFilter(None))
+@default_router.message(Command(commands=['start']), StateFilter(None))
 async def start_handler(message: Message, bot: Bot):
     await message.answer(text='Привет тебе!', parse_mode=None)
 
 
-@default_router.message.register(Command(commands=['help']), StateFilter(None))
+@default_router.message(Command(commands=['help']), StateFilter(None))
 async def help_handler(message: Message, bot: Bot):
     await message.answer(text='Я нахожу в разработке, умею только здороваться.', parse_mode=None)
 
 
-@default_router.message.register()
+@default_router.message()
 async def delete_message_handler(message: Message):
     """
     Удаляет сообщение пользователя.
