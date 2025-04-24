f86da5a36332da4fd48b1c9e66e96471973c8a0c
Andrey Bokarev <gagamen2004@gmail.com>
2025-04-24 22:43:35 +0300

    testing

--- bot/handlers/test_handlers.py
+++ bot/handlers/test_handlers.py
@@ -17,14 +17,14 @@ async def test_command_handler(message: Message):
     await message.answer('Вот тебе клавиатура', reply_markup=test_keyboard)
 
 
-@test_router.inline_query(F.data == 'testing')
+@test_router.callback_query(F.data == 'testing')
 async def test_callback_handler(callback: CallbackQuery):
     await callback.answer()
     logging.info('Вошел в функцию test_callback_handler')
     await callback.message.answer('Тестовое сообщение, запрос сработал')
 
 
-@test_router.inline_query()
+@test_router.callback_query()
 async def all_callback_handler(callback: CallbackQuery):
     logging.info(f'Получен коллбэк с данными {callback.data}')
     await callback.answer()
