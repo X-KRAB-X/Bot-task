467b676f287ce8b45eb2aff1ed2c0bd3551a12fd
Andrey Bokarev <gagamen2004@gmail.com>
2025-04-25 14:57:19 +0300

    default text changed

--- bot/handlers/default_handlers.py
+++ bot/handlers/default_handlers.py
@@ -8,13 +8,20 @@ default_router = Router(name='default_router')
 
 
 @default_router.message(Command(commands=['start']), StateFilter(None))
-async def start_handler(message: Message, bot: Bot):
-    await message.answer(text='Привет тебе!', parse_mode=None)
+async def start_handler(message: Message):
+    await message.answer(text='Привет тебе! Можешь написать /help или выбрать команду снизу.', parse_mode=None)
 
 
 @default_router.message(Command(commands=['help']), StateFilter(None))
-async def help_handler(message: Message, bot: Bot):
-    await message.answer(text='Я нахожу в разработке, умею только здороваться.', parse_mode=None)
+async def help_handler(message: Message):
+    await message.answer(text='Привет!\nУ меня есть такой список команд:\n'
+                              '/start - Поздороваюсь с тобой еще раз!\n'
+                              '/help - Сейчас именно она. Показываю справку.\n'
+                              '/get - Отправляю запрос по API.\n'
+                              '1.Даю на выбор параметры, из которых будет составлен URL.\n'
+                              '2.Сохраняю результат запроса в базе данных.\n'
+                              '3.Показываю что получилось, в формате JSON.'
+                         )
 
 
 @default_router.message()
